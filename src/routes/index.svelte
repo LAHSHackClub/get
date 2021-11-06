249430a17d8d058c30194d7102edea6204a902a9
SDBagel <sdbagel@gmail.com>
2021-11-05 23:59:23 -0700

    Add font, cover page, content

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,66 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<header>
+  <h1 class="container">
+    <span class="caption">LAHS Hack Club</span>
+    <span>GET</span>
+  </h1>
+</header>
+<main class="container">
+  <h2>A Web Presence for All Clubs</h2>
+  <br>
+  <p>Any Los Altos High School club can get a <b>branded web presence for free!</b> We sponsor a variety of options and support for every club, whether you have a website already or not.</p>
+  <br>
+  <h2>Flexible Options</h2>
+  <div class="flex-smart flex-gap">
+    <div class="flex">
+      <h3>Keep Your Site</h3>
+      <p>Keep your existing Google, Wix, or other site and we'll give you a <b>free lahs.club</b> domain as a fast shortcut.</p>
+    </div>
+    <div class="flex">
+      <h3>Make a New Site</h3>
+      <p>Learn to make and manage a new site without code with help from one of our members!</p>
+    </div>
+    <div class="flex">
+      <h3>Advanced Site</h3>
+      <p>Need more, or already have code to deploy? We can help! Requires dual club membership.</p>
+    </div>
+  </div>
+</main>
+
+<style>
+  header {
+    background: #34a;
+    padding: 60px 0;
+  }
+  h1 {
+    color: #fff;
+    line-height: 1;
+  }
+  h1 span {
+    display: block;
+    font-size: 10rem;
+  }
+  h1 span.caption {
+    font-size: 3rem;
+  }
+  h2, h3 {
+    margin-top: 25px;
+    margin-bottom: 5px;
+  }
+  h2 {
+    background-color: #34a;
+    border-radius: 5px;
+    color: #fff;
+    font-size: 1.8rem;
+    max-width: fit-content;
+    margin-top: 40px;
+    padding: 5px 20px;
+  }
+  h3 {
+    font-size: 1.5rem;
+  }
+  p {
+    font-size: 1.3rem;
+    margin: 0;
+  }
+</style>
