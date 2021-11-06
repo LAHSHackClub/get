82f28f4b277566d3f53e6a54eab6a19a738b7796
SDBagel <sdbagel@gmail.com>
2021-11-06 00:17:16 -0700

    Edit copy, add CTA button

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,7 +8,7 @@
 <main class="container">
   <h2>A Web Presence for All Clubs</h2>
   <br>
-  <p>Any Los Altos High School club can get a <b>branded web presence for free!</b> We sponsor a variety of options and support for every club, whether you have a website already or not.</p>
+  <p>Any Los Altos High School club can get a <b>branded web presence for free!</b> We sponsor a variety of options and support for every club, whether you have a website already or not. All of our options come with a <b>free lahs.club</b> domain, which is super easy and fast to remember.</p>
   <br>
   <h2>Flexible Options</h2>
   <div class="flex-smart flex-gap">
@@ -25,6 +25,7 @@
       <p>Need more, or already have code to deploy? We can help! Requires dual club membership.</p>
     </div>
   </div>
+  <p class="cta"><a href="">Submit a Request ></a></p>
 </main>
 
 <style>
@@ -62,5 +63,22 @@
   p {
     font-size: 1.3rem;
     margin: 0;
+  }
+  .cta {
+    background-color: #e38;
+    border-radius: 10px;
+    font-weight: bold;
+    font-size: 1.8rem;
+    margin: 80px 0;
+    padding: 25px 60px;
+    width: fit-content;
+    transition-duration: 0.2s;
+  }
+  .cta:hover {
+    box-shadow: #e38 0 0 20px;
+  }
+  .cta a {
+    color: #fff;
+    padding: 30px;
   }
 </style>
